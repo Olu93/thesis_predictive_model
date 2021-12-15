211d5bb929f63fb2df31acf9ac3313b598a95dfd
Olusanmi Hundogan <o.hundogan@gmail.com>
2021-12-16 00:29:45 +0100

    Fix: Some fixes

--- readers/BPIC12.py
+++ readers/BPIC12.py
@@ -4,25 +4,25 @@ import random
 
 class BPIC12W(AbstractProcessLogReader):
     class subsets:
-        A = ['A_']
-        W = ['W_']
-        O = ['O_']
-        AW = ['A_', 'W_']
-        AO = ['A_', 'O_']
-        WO = ['W_', 'O_']
-        AWO = ['A_', 'W_', 'O_']
+        A = ('A_')
+        W = ('W_')
+        O = ('O_')
+        AW = ('A_', 'W_')
+        AO = ('A_', 'O_')
+        WO = ('W_', 'O_')
+        AWO = ('A_', 'W_', 'O_')
     
 
     def __init__(self, **kwargs) -> None:
         super().__init__(log_path ='data/financial_log.xes', csv_path ='data/financial_log.csv', **kwargs)
-        self.subsets = kwargs.get('subsets', BPIC12W.subsets.AW)
+        self.subset = kwargs.get('subsets', BPIC12W.subsets.AW)
 
     def preprocess_level_general(self):
         super().preprocess_level_general(remove_cols=[])
 
     def preprocess_level_specialized(self, **kwargs):
         self.data = self.data[self.data['lifecycle:transition']=='COMPLETE']
-        self.data = self.data[self.data[self.activityId].str.startswith(self.subsets, na=False)]
+        self.data = self.data[self.data[self.activityId].str.startswith(self.subset, na=False)]
     
     
 if __name__ == '__main__':
