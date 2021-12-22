399c5d12ed615ba98cfe1ca52aaad1226b077550
Olusanmi Hundogan <o.hundogan@gmail.com>
2021-12-22 22:20:17 +0100

    Minor change

--- runner/Run_Small.py
+++ runner/Run_Small.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     batch_size = 10
     adam_init = 0.001
     num_instances = {"num_train": 1000, "num_val": 100, "num_test": 1000}
-    r1 = Runner(data, Seq2seqLSTMModelOneWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
-    r1 = Runner(data, SimpleLSTMModelOneWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
-    r2 = Runner(data, SimpleLSTMModelTwoWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
-    r3 = Runner(data, TransformerModelOneWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
-    r4 = Runner(data, TransformerModelTwoWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
+    r = Runner(data, Seq2seqLSTMModelOneWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
+    r = Runner(data, SimpleLSTMModelOneWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
+    r = Runner(data, SimpleLSTMModelTwoWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
+    r = Runner(data, TransformerModelOneWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
+    r = Runner(data, TransformerModelTwoWay(data.vocab_len, data.max_len), epochs, batch_size, adam_init, **num_instances).get_results_from_model().save_csv(folder, "test")
